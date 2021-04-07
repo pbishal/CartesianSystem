5fd34f02614a5b832cf0fb82aece71baa83b5e0f
pbishal <pbishal60@gmail.com>
2021-04-08 00:49:41 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CartesianSyayem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to cartesian system problem");
+        }
+    }
+}
