3c3c3fbb8101bfcaf22613a1f55ba971dd424a7b
pbishal <pbishal60@gmail.com>
2021-04-08 01:17:57 +0530

    added cartesian system

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace CartesianSyayem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to cartesian system problem");
+            Console.WriteLine("Enter the value of x1: ");
+            string x1val = Console.ReadLine();
+            int x1 = Convert.ToInt32(x1val);
+
+            Console.WriteLine("Enter the value of x2: ");
+            string x2val = Console.ReadLine();
+            int x2 = Convert.ToInt32(x2val);
+
+            Console.WriteLine("Enter the value of y1: ");
+            string y1val = Console.ReadLine();
+            int y1 = Convert.ToInt32(y1val);
+
+            Console.WriteLine("Enter the value of y2: ");
+            string y2val = Console.ReadLine();
+            int y2 = Convert.ToInt32(y2val);
+
+            double lengthOfALine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("The length is: " + lengthOfALine);
+            Console.ReadLine();
         }
     }
 }
