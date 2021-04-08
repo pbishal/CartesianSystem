63503a83dce1a723fb5fac7a0f9b70df0e59ff13
pbishal <pbishal60@gmail.com>
2021-04-08 22:03:52 +0530

    Shows two lines are equal or not

--- Program.cs
+++ Program.cs
@@ -22,8 +22,29 @@ namespace CartesianSyayem
             string y2val = Console.ReadLine();
             int y2 = Convert.ToInt32(y2val);
 
-            double lengthOfALine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            Console.WriteLine("The length is: " + lengthOfALine);
+            Console.WriteLine("Enter the value of p1: ");
+            string p1val = Console.ReadLine();
+            int p1 = Convert.ToInt32(p1val);
+
+            Console.WriteLine("Enter the value of p2: ");
+            string p2val = Console.ReadLine();
+            int p2 = Convert.ToInt32(p2val);
+
+            Console.WriteLine("Enter the value of q1: ");
+            string q1val = Console.ReadLine();
+            int q1 = Convert.ToInt32(q1val);
+
+            Console.WriteLine("Enter the value of q2: ");
+            string q2val = Console.ReadLine();
+            int q2 = Convert.ToInt32(q2val);
+
+            double lengthOfLine1 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("The length of line 1 is: " + lengthOfLine1);
+
+            double lengthOfLine2 = Math.Sqrt(Math.Pow((p2 - p1), 2) + Math.Pow((q2 - q1), 2));
+            Console.WriteLine("The length of line 2 is: " + lengthOfLine2);
+
+            Console.WriteLine(lengthOfLine1.Equals(lengthOfLine2));
             Console.ReadLine();
         }
     }
